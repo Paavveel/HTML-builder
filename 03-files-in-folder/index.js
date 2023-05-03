1e1ff5feab70830d9cb4a8eb190948b67d9c3860
Paavveel <pa.tomilin@gmail.com>
2023-05-03 21:21:27 +0300

    feat: complete 03-files-in-folder task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,23 @@
+const fs = require('node:fs');
+const fsPromises = require('node:fs/promises');
+const path = require('node:path');
+
+const pathToFolder = path.join(__dirname, 'secret-folder');
+
+fsPromises.readdir(pathToFolder, { withFileTypes: true }).then((files) => {
+  for (const file of files) {
+    if (file.isFile()) {
+      const pathToFile = path.join(pathToFolder, file.name);
+
+      fs.stat(pathToFile, (err, stats) => {
+        if (err) throw err;
+        if (stats.isFile()) {
+          const fileType = path.extname(file.name).slice(1);
+          const fileSize = stats.size / 1024;
+          const fileName = path.parse(pathToFile).name;
+          console.log(`${fileName} - ${fileType} - ${fileSize}kb`);
+        }
+      });
+    }
+  }
+});
